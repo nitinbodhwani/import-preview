1dffcca6043cea4cef4b9b96ef95d962db5bad3e
Ruthvika R <ruthvika.r@kdi.kongsberg.com>
2018-03-28 13:52:31 +0530

    Routing and favicon changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import { AppComponent } from './app.component';
-import {MainPanelComponent} from './components/mainPanel/mainPanel.component';
-import {ImportSectionComponent} from './components/importSection/importSection.component';
-import {XLSXDirective} from './directives/xlsx.directive';
-import { DataTableComponent } from './components/data-table/data-table.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './/app-routing.module';
 import { NgTableComponent, NgTableFilteringDirective, NgTablePagingDirective, NgTableSortingDirective, Ng2TableModule } from 'ng2-table';
-import {ImportTemplateComponent} from './components/importTemplate/importTemplate.component';
+
+import { AppComponent } from './app.component';
+import { MainPanelComponent } from './components/mainPanel/mainPanel.component';
+import { ImportSectionComponent } from './components/importSection/importSection.component';
+import { XLSXDirective } from './directives/xlsx.directive';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { ImportTemplateComponent} from './components/importTemplate/importTemplate.component';
+import { ReportPreviewComponent } from './components/reportPreview/reportPreview.component';
 
 
 @NgModule({
@@ -17,14 +20,16 @@ import {ImportTemplateComponent} from './components/importTemplate/importTemplat
     ImportSectionComponent,
     XLSXDirective,
     DataTableComponent,
-    ImportTemplateComponent
+    ImportTemplateComponent,
+    ReportPreviewComponent
     //NgTableComponent, NgTableFilteringDirective, NgTablePagingDirective, NgTableSortingDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
     //NgbModule.forRoot(),
-    Ng2TableModule
+    Ng2TableModule,
+    AppRoutingModule
   ],
   providers: [],
 
