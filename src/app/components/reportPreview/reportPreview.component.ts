1f5bc924026825c824d400e76df67c2eb322787c
Ruthvika R <ruthvika.r@kdi.kongsberg.com>
2018-03-29 18:30:12 +0530

    implementation of month and year

--- src/app/components/reportPreview/reportPreview.component.ts
+++ src/app/components/reportPreview/reportPreview.component.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
- 
+import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http'; 
+import 'rxjs/add/operator/map';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 
 @Component({
@@ -7,8 +9,43 @@ import {Component} from '@angular/core';
     templateUrl:'./reportPreview.component.html'
 })
 
-export class ReportPreviewComponent{
+export class ReportPreviewComponent implements OnInit {
     currentJustify = 'justified';
     selectedByDate: any;
     selectedByCard: any;
+    monthArray:string[]=[
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    selectedMonth:string="January"; 
+    minYear:number;   
+    maxYear:number;        
+
+    constructor(private http:Http){
+
+    }
+
+    ngOnInit(){
+        this.http.get('/assets/config.json')
+        .map(data => data.json())
+        .subscribe(data=>{
+            this.minYear=data.startYear;
+            this.maxYear=data.maxYear;
+            console.log(this.minYear);
+        })
+    }
+
+    SearchByCard(){
+        console.log(this.selectedMonth);
+    }
 }
