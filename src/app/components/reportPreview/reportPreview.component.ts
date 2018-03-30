c8cff7cfe2c091c4ead5e07ae7d5c0ca4ad04433
Ruthvika R <ruthvika.r@kdi.kongsberg.com>
2018-03-30 11:21:40 +0530

    Pre populated the year drop down

--- src/app/components/reportPreview/reportPreview.component.ts
+++ src/app/components/reportPreview/reportPreview.component.ts
@@ -28,24 +28,34 @@ export class ReportPreviewComponent implements OnInit {
         "December"
     ];
     selectedMonth:string="January"; 
+    selectedYear:number=2018;
     minYear:number;   
-    maxYear:number;        
+    maxYear:number;   
+    years:number[];     
 
     constructor(private http:Http){
 
     }
 
     ngOnInit(){
+        this.years=[];
         this.http.get('/assets/config.json')
         .map(data => data.json())
         .subscribe(data=>{
             this.minYear=data.startYear;
             this.maxYear=data.maxYear;
-            console.log(this.minYear);
+            console.log(this.minYear+"  "+this.maxYear);
+            for(let i = this.minYear; i<=this.maxYear;i++){
+                this.years.push(i);
+            }
         })
     }
 
     SearchByCard(){
         console.log(this.selectedMonth);
+        console.log(this.selectedYear);
+    }
+    SearchByDate(){
+        console.log(this.selectedByDate);
     }
 }
